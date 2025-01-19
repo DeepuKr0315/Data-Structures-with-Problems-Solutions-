d9f3837181591a15b667f744a20a2bd4d38d55cd
DeepuKr0315 <125431399+DeepuKr0315@users.noreply.github.com>
2025-01-19 15:47:55 +0530

    adding sorting files

--- 10. Sorting/insertionSort.py
+++ 10. Sorting/insertionSort.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def insertionSort(nums):
     for i in range(1, len(nums)):
         j = i-1
@@ -8,4 +9,16 @@ def insertionSort(nums):
         nums[j+1] = temp
     return nums
 
+=======
+def insertionSort(nums):
+    for i in range(1, len(nums)):
+        j = i-1
+        temp = nums[i]
+        while j >= 0 and nums[j] > temp:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = temp
+    return nums
+
+>>>>>>> eca4169 (adding sorting files)
 print(insertionSort([5, 3, 4, 8, 1]))
--- 10. Sorting/mergeSort.py
+++ 10. Sorting/mergeSort.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def mergeSort(nums):
     if len(nums) <= 1:
         return nums
@@ -26,4 +27,34 @@ def merge(nums1, nums2):
         j += 1
     return mergedArr
 
+=======
+def mergeSort(nums):
+    if len(nums) <= 1:
+        return nums
+    middle = len(nums)//2
+    leftSide = mergeSort(nums[:middle])
+    rightSide = mergeSort(nums[middle:]) 
+    return merge(leftSide, rightSide)
+
+def merge(nums1, nums2):
+    mergedArr = []
+    i, j = 0, 0
+    m = len(nums1)
+    n = len(nums2)
+    while i < m and j < n:
+        if nums1[i] < nums2[j]:
+            mergedArr.append(nums1[i])
+            i += 1
+        else:
+            mergedArr.append(nums2[j])
+            j += 1
+    while i < m:
+        mergedArr.append(nums1[i])
+        i += 1
+    while j < n:
+        mergedArr.append(nums2[j])
+        j += 1
+    return mergedArr
+
+>>>>>>> eca4169 (adding sorting files)
 print(mergeSort([5,4,3,2,1]))
