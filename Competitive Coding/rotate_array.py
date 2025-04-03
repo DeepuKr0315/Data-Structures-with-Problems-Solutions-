8208e57207fa653b639f6f13352faa2d63484a1a
DeepuKr0315 <125431399+DeepuKr0315@users.noreply.github.com>
2025-04-04 00:14:07 +0530

    adding code file

--- Competitive Coding/rotate_array.py
+++ Competitive Coding/rotate_array.py
@@ -1,6 +1,6 @@
 def rotate_array(nums, k):
 
-    def helper(left, right):
+    def rotate(left, right):
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
@@ -8,9 +8,9 @@ def rotate_array(nums, k):
         return nums
     
     k = k % len(nums)
-    helper(0, len(nums)-1)
-    helper(0, k - 1)
-    helper(k, len(nums) - 1)
+    rotate(0, len(nums)-1)
+    rotate(0, k - 1)
+    rotate(k, len(nums) - 1)
     return nums
 
 print(rotate_array([6, 8, 1, 4, 9], 2))
