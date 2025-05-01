0a7de401f4f625d20717310244ae0e9f68c40ecd
DeepuKr0315 <125431399+DeepuKr0315@users.noreply.github.com>
2025-05-01 23:53:20 +0530

    adding new file

--- 3. Dynamic Programming/931. Minimum Falling Path Sum/memoization.py
+++ 3. Dynamic Programming/931. Minimum Falling Path Sum/memoization.py
@@ -3,12 +3,16 @@
 
 def minFalling(matrix):
     N = len(matrix)
+    check = {}
     def helper(row, col):
         if(row == N):
             return 0
         if(col == N or col < 0):
             return float('inf')
+        if (row, col) in check:
+            return check[(row, col)]
         result = matrix[row][col] + min(helper(row + 1, col - 1), helper(row + 1, col), helper(row + 1, col + 1))
+        check[(row, col)] = result
         return result
     result = float('inf')
     for col in range(N):
