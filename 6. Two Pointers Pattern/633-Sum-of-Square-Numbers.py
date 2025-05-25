184e61b2d9c9adca98dc887b2d726ab49c74634f
Deepanshu Kumar <deepanshukumar@Deepanshus-MacBook-Air.local>
2025-05-25 22:37:33 +0530

    633. Sum of Square Numbers

---
+++ 6. Two Pointers Pattern/633-Sum-of-Square-Numbers.py
@@ -0,0 +1,29 @@
+# Problem link = 633. Sum of Square Numbers
+
+def judgeSquareSum(num):
+    def sqrt(num):
+        left = 0
+        right = num
+        ans = 0
+        while left < right:
+            mid = (left + right) // 2
+            if mid * mid <= num:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
+    left = 0
+    right = sqrt(num)
+    while left <= right:
+        total = left * left + right * right
+        if total == num:
+            return True
+        elif total > num:
+            right -= 1
+        else:
+            left += 1
+    return False
+
+c = 5
+print(judgeSquareSum(c))
