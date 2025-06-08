281bffcb282f763d7f31ae1d5826571a744eb641
DeepuKr0315 <deepanshukumar@Deepanshus-MacBook-Air.local>
2025-06-08 12:38:58 +0530

    386. Lexicographical Numbers

---
+++ Daily Problems/386-Lexicographical-numbers.py
@@ -0,0 +1,15 @@
+def lexicographical_num(n):
+    res = [] 
+    def dfs(num):
+        if num > n:
+            return
+        res.append(num)
+        for i in range(10):
+            if num * 10 + i > n:
+                return
+            dfs(num * 10 + i)
+    for i in range(1, 10):
+        dfs(i)
+    return res
+
+print(lexicographical_num(5))
