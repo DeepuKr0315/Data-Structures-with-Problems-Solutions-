29c9a74395b0da570954c558eec302712dddc04c
DeepuKr0315 <deepanshukumar@Deepanshus-MacBook-Air.local>
2025-06-16 22:34:46 +0530

    Selection Sort

--- 10. Sorting/selectionSort.py
+++ 10. Sorting/selectionSort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def selectionSort(nums):
     # The outer loop run n times, where n is the length of nums
     # As i increase, the nuber of times inner loop runs decreases
@@ -13,20 +12,4 @@ def selectionSort(nums):
             nums[i], nums[smallest] = nums[smallest], nums[i]
     return nums
 
-=======
-def selectionSort(nums):
-    # The outer loop run n times, where n is the length of nums
-    # As i increase, the nuber of times inner loop runs decreases
-    # Therefore, this loop will run roughly n*(n-1)/2 times, which simplifies to O(n^2)
-    for i in range(len(nums)):
-        smallest = i
-        for j in range(i + 1, len(nums)):
-            if nums[j] < nums[smallest]:
-                smallest = j
-        # The swap operation done in constant time O(1)
-        if i != smallest:
-            nums[i], nums[smallest] = nums[smallest], nums[i]
-    return nums
-
->>>>>>> eca4169 (adding sorting files)
 print(selectionSort([5, 4, 3, 2, 1]))
