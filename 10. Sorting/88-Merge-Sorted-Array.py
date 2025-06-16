2a9c9f8f4228d7ccceab679ba620b542efc656c9
DeepuKr0315 <deepanshukumar@Deepanshus-MacBook-Air.local>
2025-06-16 22:36:47 +0530

    updating files

---
+++ 10. Sorting/88-Merge-Sorted-Array.py
@@ -0,0 +1,26 @@
+
+
+# problem link = https://leetcode.com/problems/merge-sorted-array/description/
+
+def merge(nums1, m, nums2, n):
+    mergedArr = []
+    i, j = 0, 0
+    while i < m and j < n:
+        if nums1[i] < nums2[j]:
+            mergedArr.append(nums1[i])
+            i += 1
+        else:
+            mergedArr.append(nums2[j])
+            j += 1
+    while i < m:
+        mergedArr.append(nums1[i])
+        i += 1
+    while j < n:
+        mergedArr.append(nums2[j])
+        j += 1
+    for i in range(len(mergedArr)):
+        nums1[i] = mergedArr[i]
+
+    return nums1
+
+print(merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3))
--- 10. Sorting/radixSort.py
+++ 10. Sorting/radixSort.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+# Radix Sort Implementation
 def radixSort(array):
     if len(array) == 0:
         return 'empty array'
@@ -24,31 +24,4 @@ def counting_sort(array, place):
         output[insert_position] = array[j]
     array[:] = output[:]
 
-=======
-def radixSort(array):
-    if len(array) == 0:
-        return 'empty array'
-    greatest_number = max(array)
-    number_of_digits = len(str(greatest_number))
-    for i in range(number_of_digits):
-        counting_sort(array, i)
-    return array
-
-def counting_sort(array, place):
-    output = [0]*len(array)
-    temp = [0] * 10
-    digit_place = 10 ** place
-    for num in array:
-        digit = (num // digit_place) % 10
-        temp[digit] += 1
-    for i in range(1, 10):
-        temp[i] += temp[i-1]
-    for j in range(len(array) - 1, -1, -1):
-        curr_digit = (array[j] // digit_place) % 10
-        temp[curr_digit] -= 1
-        insert_position = temp[curr_digit]
-        output[insert_position] = array[j]
-    array[:] = output[:]
-
->>>>>>> c56d9ed (adding more sorting codes)
 print(radixSort([12,24,567,457,227,88,23,72]))
