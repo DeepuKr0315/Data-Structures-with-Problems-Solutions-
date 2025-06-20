8bbf89714882a569ad2e4d8db0028809f4789dbc
DeepuKr0315 <deepanshukumar@Deepanshus-MacBook-Air.local>
2025-06-20 08:03:22 +0530

    5. Longest Palindromic Substring

--- 3. Dynamic Programming/Palindromc Substring/5-Longest-Palindromic-Substring.py
+++ 3. Dynamic Programming/Palindromc Substring/5-Longest-Palindromic-Substring.py
@@ -20,3 +20,25 @@ def longestPalindrome(s):
 
 s = "babad"
 print(longestPalindrome(s))
+
+# 2 pointers approach
+def longestPalindromeTwoPointers(s):
+    def expand(left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1 : right]
+    longest = ""
+    for i in range(len(s)):
+        # odd length in palindrome
+        odd_palindrome = expand(i, i)
+        if len(odd_palindrome)  > len(longest):
+            longest = odd_palindrome
+        # even length palindrome
+        even_palindrome = expand(i, i + 1)
+        if len(even_palindrome) > len(longest):
+            longest = even_palindrome
+    return longest
+
+s = "babad"
+print(longestPalindromeTwoPointers(s))
