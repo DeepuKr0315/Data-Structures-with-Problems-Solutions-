85639b0f260ca2183b628c5939237e715ec3a459
Deepanshu Kumar <deepanshukr0315@gmail.com>
2025-07-02 23:02:07 +0530

    5. Longest Palindromic Substring

--- 3. Dynamic Programming/647-Palindromic-Substrings.py
+++ 3. Dynamic Programming/647-Palindromic-Substrings.py
@@ -27,5 +27,25 @@ def countPalindrome(s):
 
     return res
 
+def countPalindrome_aproach2(s):
+    n = len(s)
+    dp = [[0]*(n) for _ in range(n)]
+    result = 0
+    for l in range(1, n+1):
+        for i in range(n-l+1):
+            j = i+l-1
+            if i == j:
+                dp[i][j] = True
+                result += 1
+            elif s[i] == s[j] and (j == i+1 or dp[i+1][j-1]):
+                dp[i][j] = True
+                result += 1
+            else:
+                dp[i][j] = False
+    return result
+
 s = "abc"
 print(countPalindrome(s))
+
+s = "abc"
+print(countPalindrome_aproach2(s))
