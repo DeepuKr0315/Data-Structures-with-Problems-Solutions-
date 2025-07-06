9a695942e650ffc9eb401511c8d873ba8a92286b
Deepanshu Kumar <deepanshukr0315@gmail.com>
2025-07-06 22:36:05 +0530

    Shortest Common Supersequence using tabulation

---
+++ 3. Dynamic Programming/Shortest Common Supersequence/tabulation_method.py
@@ -0,0 +1,18 @@
+def shortest_common_supersequence(s1, s2):
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+    for i in range(len(s1) + 1):
+        dp[i][0] = i
+    for j in range(len(s2) + 1):
+        dp[0][j] = j
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1])
+    return dp[-1][-1]
+
+# Example usage
+s1 = "geek"
+s2 = "eke"
+print("Length of Shortest Common Supersequence is", shortest_common_supersequence(s1, s2))
