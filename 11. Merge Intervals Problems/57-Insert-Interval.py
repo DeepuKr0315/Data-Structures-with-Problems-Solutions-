4628d499b2cc9f28f727fd7fe442e101cd9f4d77
Deepanshu Kumar <deepanshukr0315@gmail.com>
2025-07-10 13:20:46 +0530

    57. Insert Interval

--- 11. Merge Intervals Problems/57-Insert-Interval.py
+++ 11. Merge Intervals Problems/57-Insert-Interval.py
@@ -5,7 +5,7 @@ def insert(intervals, newInterval):
     res = []
     for i in range(len(intervals)):
         if newInterval[1] < intervals[i][0]:
-            res.appen(newInterval)
+            res.append(newInterval)
             return res + intervals[i:]
         elif newInterval[0] > intervals[i][1]:
             res.append(intervals[i])
