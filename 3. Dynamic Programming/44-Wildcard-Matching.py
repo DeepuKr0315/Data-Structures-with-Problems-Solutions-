5a771ae5a8310fb4efaecdcbd01ad796465cbeb9
Deepanshu Kumar <deepanshukr0315@gmail.com>
2025-08-18 10:05:06 +0530

    44. Wildcard Matching

---
+++ 3. Dynamic Programming/44-Wildcard-Matching.py
@@ -0,0 +1,23 @@
+# Problem link = https://leetcode.com/problems/wildcard-matching/
+
+def isMtach_recursion(s1, s2):
+    def helper(idx1, idx2):
+        if idx1 == len(s1) and idx2 == len(s2):
+            return True
+        if idx2 == len(s2):
+            return False
+        if idx1 == len(s1):
+            for i in range(idx2, len(s2)):
+                if s2[i] != '*':
+                    return False
+            return True
+        if s1[idx1] == s2[idx2] or s2[idx2] == '?':
+            return helper(idx1 + 1, idx2 + 1)
+        if s2[idx2] == '*':
+            return helper(idx1 + 1, idx2) or helper(idx1, idx2 + 1)
+        return False
+    return helper(0, 0)
+
+s1 = "aa"
+s2 = "a"
+print(isMtach_recursion(s1, s2)) 
